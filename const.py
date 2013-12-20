ad256702504b5c9996d745191900029df816aaae
郭威 <apanly@163.com>
2013-12-20 23:54:51 +0800

    Update const.py

--- const.py
+++ const.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-CONST_HOSTADDR= '130.bca.myftpupload.com' # ftp地址
-CONST_USERNAME='g1307798' # 用户名
-CONST_PASSWORD='TMxsd12s%@1' # 密码
+CONST_HOSTADDR= 'xxx' # ftp地址
+CONST_USERNAME='yyy' # 用户名
+CONST_PASSWORD='zzz' # 密码
 CONST_PORT=21
 CONST_ROOTPATH='/home/vincent/opensource/simpleenglish/'
 CONT_PATHPREFIX='/'
